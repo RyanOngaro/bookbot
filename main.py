b8e23bdc1bec49320ae184b5bfff83613f953583
RyanOngaro <68353478+RyanOngaro@users.noreply.github.com>
2025-04-14 19:13:33 -0400

    'Count Words' task

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def word_count(book_string):
+    words = book_string.split()
+    return len(words)
+
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    book_path = "books/frankenstein.txt"
+    raw_text = get_book_text(book_path)  
+    count = word_count(raw_text)
+    print(f"{count} words found in the document")
 
 main()
