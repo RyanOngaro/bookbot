24221813c54b7d961ac84f9a020018acd85d4337
RyanOngaro <68353478+RyanOngaro@users.noreply.github.com>
2025-04-18 13:43:55 -0400

    'Count Characters' task

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def get_book_text(filepath):
 def main():
     book_path = "books/frankenstein.txt"
     raw_text = get_book_text(book_path)  
-    count = word_count(raw_text)
-    print(f"{count} words found in the document")
+    wcount = word_count(raw_text)
+    ccount = char_count(raw_text)
+    print(ccount)
+    ## print(f"{wcount} words found in the document")
 
 main()
--- stats.py
+++ stats.py
@@ -2,5 +2,13 @@ def word_count(book_string):
     words = book_string.split()
     return len(words)
 
-def char_count(book_string)
-    
+def char_count(book_string):
+    characters = {}
+    chars = list(book_string.lower())
+    for char in chars:
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+    return characters
+    ## characters["x"] += 1
